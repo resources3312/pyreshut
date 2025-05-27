3ad0ab8b3bab69e1fdfb6ad199f8993640a9acb5
ViCoder <145913485+resources3312@users.noreply.github.com>
2025-05-27 15:37:24 +0300

    Update pyreshut.c

--- src/pyreshut/pyreshut.c
+++ src/pyreshut/pyreshut.c
@@ -2,15 +2,8 @@
 Pyreshut 1.0
 Coded by: ViCoder32
 
-    Simple or even primitive library for reboot or
-    shutdown pc with using low-level code on my
-    favourite C. In next version I will update this
-    library and add new functional, but while it`s 
-    basic kit for control most low level your pc, 
-    I don`t know why nobody written library, which
-    I written for 1 hour with beer and berserk on 
-    monitor. Fuck, 45 strings in python file, 
-    and 38 strings in C file
+    Simple for reboot or
+    shutdown pc with using C language.
  */
 #include <unistd.h>
 #define REBOOT 0x1234567 
